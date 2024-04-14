6d9abc28146ecc1628d7e75702dd68be2bd14988
Amirhossein Zandi <blackviper933@gmail.com>
2024-04-14 20:22:08 +0330

    "Remove unused imports from IntegrationsRegistry.tsx"
    Explanation:
    The diff shows that several imports from @mui/material and @mui/icons-material have been removed from the file IntegrationsRegistry.ts

--- app/integrations/IntegrationsRegistry.tsx
+++ app/integrations/IntegrationsRegistry.tsx
@@ -1,8 +1,4 @@
-import { Box, Button, InputAdornment, Stack, TextField, Typography } from "@mui/material";
-import AddIcon from '@mui/icons-material/Add';
-import UploadIcon from '@mui/icons-material/Upload';
-import DownloadIcon from '@mui/icons-material/Download';
-import SearchIcon from '@mui/icons-material/Search';
+import { Box } from "@mui/material";
 import CustomersInput from "../customers/CustomersInput/CustomersInput";
 
 
