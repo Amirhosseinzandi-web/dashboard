62079abd1ed0acf249b65739bd5772b06721f609
Amirhossein Zandi <blackviper933@gmail.com>
2024-04-15 21:32:36 +0330

    Update NotFound component with HTML entity for apostrophe in typography text
    This commit updates the NotFound component by replacing the apostrophe in the typography text with the HTML entity code `&apos;`. This ensures that the apost

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,7 +13,9 @@ const NotFound = () => {
                     <figure>
                         <Image alt="error" width={400} height={400} src={"/error.png"} priority={false} />
                     </figure>
-                    <Typography variant="h4" textAlign={"center"} mt={2}>404: The page you are looking for isn't here</Typography>
+                    <Typography variant="h4" textAlign={"center"} mt={2}>
+                        404: The page you are looking for isn&apos;t here
+                    </Typography>
                     <Typography variant="h6" textAlign={"center"} color={"#667085"} fontSize={16} my={2}>You either tried some shady route or you came here by mistake. Whichever it is, try using the navigation</Typography>
                     <Link href={"/dashboard"}>
                         <Button variant="contained" sx={{ bgcolor: "#635BFF", color: "white", borderRadius: 3, textTransform: "capitalize", ":hover": { bgcolor: "#4e36f5" }, display: "flex", gap: 1 }}>
